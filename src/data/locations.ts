1ad2d96e16c58fb8108b6151f00c6cb04f91abd2
gokcekmustafa <gokcek@outlook.com>
2026-03-25 22:28:51 +0300

    Update locations.ts

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,1 +1,15 @@
-
+export const locations = [
+  {
+    city: "İstanbul",
+    regions: [
+      { name: "Avrupa Yakası", districts: ["Arnavutköy", "Avcılar", "Bağcılar", "Bahçelievler", "Bakırköy", "Başakşehir", "Bayrampaşa", "Beşiktaş", "Beylikdüzü", "Beyoğlu", "Büyükçekmece", "Çatalca", "Esenler", "Esenyurt", "Eyüpsultan", "Fatih", "Gaziosmanpaşa", "Güngören", "Kâğıthane", "Küçükçekmece", "Sarıyer", "Silivri", "Sultangazi", "Şişli", "Zeytinburnu"] },
+      { name: "Anadolu Yakası", districts: ["Adalar", "Ataşehir", "Beykoz", "Çekmeköy", "Kadıköy", "Kartal", "Maltepe", "Pendik", "Sancaktepe", "Sultanbeyli", "Şile", "Tuzla", "Ümraniye", "Üsküdar"] }
+    ]
+  },
+  {
+    city: "Ankara",
+    regions: [
+      { name: "Merkez ve İlçeler", districts: ["Akyurt", "Altındağ", "Ayaş", "Bala", "Beypazarı", "Çamlıdere", "Çankaya", "Çubuk", "Elmadağ", "Etimesgut", "Evren", "Gölbaşı", "Güdül", "Haymana", "Kahramankazan", "Kalecik", "Keçiören", "Kızılcahamam", "Mamak", "Nallıhan", "Polatlı", "Pursaklar", "Sincan", "Şereflikoçhisar", "Yenimahalle"] }
+    ]
+  }
+];
